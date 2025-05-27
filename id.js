64665de64041d300d5dc69ba4965ab8bd961158d
Humbah2005 <tanakapunungwe21@gmail.com>
2025-05-27 06:07:11 +0200

    Update id.js

--- id.js
+++ id.js
@@ -1,4 +1,4 @@
-function humbaid(num = 4) {
+function humbaid(num = 5`) {
   let result = "humba~";
   let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   var characters9 = characters.length;
@@ -7,4 +7,4 @@ function humbaid(num = 4) {
   }
   return result;
 }
-module.exports = {hunbaid};
+module.exports = {humbaid};
