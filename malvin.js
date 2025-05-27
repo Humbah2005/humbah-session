8f00889a33901401d2b4cbe970d19f842f818772
Humbah2005 <tanakapunungwe21@gmail.com>
2025-05-27 06:21:51 +0200

    Update malvin.js

--- malvin.js
+++ malvin.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
+const path = require('path');
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
 let code = require('./pair');
-require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/code', code);
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-    console.log(`
-Deployment Successful!
 
- Session-Server Running on http://localhost:` + PORT)
-})
+app.use('/code', code);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pair.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Deployment Successful! Session-Server Running on http:                      
+}).on('error', (err) => {
+    console.error(`//localhost:${PORT}`);
+}).on('error', (err) => {
+    console.error(`Error starting server: ${err}`);
+});
 
 module.exports = app;
-       
